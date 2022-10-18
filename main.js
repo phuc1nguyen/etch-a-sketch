51927e684dfbb909e680cf4a71f864e70b26a74f
phuc1nguyen <phuc.ng13988@gmail.com>
2022-10-18 12:29:21 +0700

    randomRgb() returns colors with full instead of random opacity

--- main.js
+++ main.js
@@ -89,17 +89,7 @@ function randomRgb() {
     r = Math.random,
     s = 255;
 
-  return (
-    "rgba(" +
-    o(r() * s) +
-    "," +
-    o(r() * s) +
-    "," +
-    o(r() * s) +
-    "," +
-    r().toFixed(1) +
-    ")"
-  );
+  return "rgb(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s) + ")";
 }
 
 // Grid
